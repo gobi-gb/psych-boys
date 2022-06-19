e1f51d8cd5417013612449a3ea1718d0b616afdd
GOBINATH G <gobichan2209@gmail.com>
2022-06-19 18:28:50 +0530

    Name update

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
     });
     return (
       <div className="App">
-        <h1 className="title">Psych Boys</h1>
+        <h1 className="title">Psych Boy</h1>
 
         <SearchBox
           className="boys-search-box"
